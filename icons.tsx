d7f7c3d2520aebce85b7af362e15073b23b86106
Maxim-Nastya <tel.9139065810@gmail.com>
2025-08-27 00:38:29 +0300

    feat: Implement SVG logo icon
    Replaces the img tag with an SVG element for the application logo. This allows for better scalability and theming of the logo.

--- icons.tsx
+++ icons.tsx
@@ -1,5 +1,8 @@
 export const LogoIcon = ({ size = 32 }: { size?: number }) => (
-    <img src="./logo.svg" alt="Логотип Прораб" width={size} height={size} className="app-logo-img" />
+    <svg width={size} height={size} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="app-logo-img">
+        <rect width="32" height="32" rx="8" fill="hsl(var(--primary-dark))"/>
+        <path d="M9 24V8H16C19.3137 8 22 10.6863 22 14C22 17.3137 19.3137 20 16 20H13V24H9Z" fill="hsl(var(--background))"/>
+    </svg>
 );
 export const EditIcon = () => <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04Z" fill="currentColor"/></svg>;
 export const DeleteIcon = () => <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM19 4H15.5L14.5 3H9.5L8.5 4H5V6H19V4Z" fill="currentColor"/></svg>;
